7644add789ccb84615e6659668007fa657aa7d08
Ruben Gonzales <rgsxivdev@gmail.com>
2025-11-06 19:34:18 -0800

    player is now visible

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
+from player import Player
 
 def main():
     print("Starting Asteroids!")
@@ -10,14 +11,16 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
+    first_player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         log_state()
+        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-
+        
         screen.fill('black')
+        first_player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
